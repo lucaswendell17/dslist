16ad83a8e473373ffb2a5b1936dd6e95718a3065
Lucas Wendel <lucas.wmartins@hotmail.com>
2023-05-11 20:30:02 -0300

    Consulta SQL, projection, games by list

--- src/main/java/com/lucasdev/dslist/services/GameService.java
+++ src/main/java/com/lucasdev/dslist/services/GameService.java
@@ -9,6 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.lucasdev.dslist.dto.GameDTO;
 import com.lucasdev.dslist.dto.GameMinDTO;
 import com.lucasdev.dslist.entities.Game;
+import com.lucasdev.dslist.projections.GameMinProjection;
 import com.lucasdev.dslist.repositories.GameRepository;
 
 
@@ -28,6 +29,12 @@ public class GameService {
 	public List<GameMinDTO> findAll(){
 		
 		List<Game> result = gameRepository.findAll();
+		return result.stream().map(x -> new GameMinDTO(x)).toList();
+	}
+	
+	@Transactional(readOnly = true)
+	public List<GameMinDTO> findByList(Long listId){
+		List<GameMinProjection> result = gameRepository.searchByList(listId);
 		return result.stream().map(x -> new GameMinDTO(x)).toList();
 	}
 }
