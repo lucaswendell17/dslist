e4605865db3b2f01003ebe1352eaaa93fb37cd3d
Lucas Wendel <lucas.wmartins@hotmail.com>
2023-05-11 19:22:47 -0300

    Get lists

---
+++ src/main/java/com/lucasdev/dslist/services/GameListService.java
@@ -0,0 +1,23 @@
+package com.lucasdev.dslist.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.lucasdev.dslist.dto.GameListDTO;
+import com.lucasdev.dslist.repositories.GameListRepository;
+
+@Service
+public class GameListService {
+
+	@Autowired
+	private GameListRepository gameListRepository;
+	
+	@Transactional(readOnly = true)
+	public List<GameListDTO> findAll(){
+		var results = gameListRepository.findAll();
+		return results.stream().map(x -> new GameListDTO(x)).toList();
+	}
+}
